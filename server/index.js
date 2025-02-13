8a8cf900ba330f252c1d59d574084193113fee97
Ines Amirat <inesamirat20@gmail.com>
2025-02-13 21:35:04 +0100

    refactoring

--- server/index.js
+++ server/index.js
@@ -1,17 +1,83 @@
 import http, { request } from 'http';
 import 'dotenv/config';
-import books_endpoints from '../src/endpoints/books.js';
+import connexion from '../config/db.js';
 
 
 const PORT = process.env.PORT || 8000;
 const server = http.createServer((req, res) => {
+      res.setHeader('Content-type', 'application/json');
 
+      // Route : Page d'accueil 
+      if ((req.url == '/' && req.method == 'GET')) {
+            res.write(JSON.stringify({ message: "Bienvenue dans l'API" }));
+            res.end();
 
-      if (req.url == '/api/books' && req.method == 'GET') {
-              books_endpoints(req,res);
       }
-      
-             
+      else {
+            // Route : Liste des livres
+            if (req.url == '/api/books' && req.method == 'GET') {
+                  connexion.query("select * from books ", function (err, result, fields) {
+                        if (err) {
+                              console.log("error");
+                              res.statusCode = 500;
+                              res.write(JSON.stringify({ message: "Server Error" }));
+                              res.end();
+                        } else {
+                              if (result != "") {
+                                    console.log(result);
+                                    res.statusCode = 200;
+                                    res.write(JSON.stringify(result));
+                                    res.end();
+                              }
+                              else {
+                                    console.log(result);
+                                    res.statusCode = 200;
+                                    res.write(JSON.stringify({ message: "no books available " }));
+                                    res.end();
+                              }
+                        }
+
+                  });
+            }
+            // Route : Ajouter un livre par l'Admin
+            else if (req.url == '/api/books' && req.method == 'POST') {
+                  let body = "";
+                  req.on('data', (chunk) => {
+                        body += chunk.toString();
+                  })
+                  req.on('end', () => {
+                        try {
+                              const newbook = JSON.parse(body);
+                              const query = "INSERT INTO books (book_id, title,  author, genre, published_year, state) values (?, ?, ?, ?, ?, ?)";
+                              const values = [newbook.book_id, newbook.title, newbook.author, newbook.genre, newbook.published_year, newbook.state];
+                              connexion.query(query, values, (err) => {
+
+                                    res.setHeader('Content-type', 'application/json');
+                                    if (err) {
+                                          res.statusCode = 500;
+                                          res.write(JSON.stringify({ message: "Error " }));
+                                    }
+                                    else {
+                                          res.statusCode = 201;
+                                          res.write(JSON.stringify({ message: "Book added successfully " }));
+
+                                    }
+                                    res.end();
+                              });
+                        }
+                        catch (error) {
+                              res.setHeader('Content-type', 'application/json');
+                              res.statusCode = 400;
+                              res.write(JSON.stringify({ message: "Invalid JSON format " }));
+                              res.end();
+
+                        }
+                  });
+            }
+      }
+
+
+
 
 });
 
